6569fe625643e53ed2a3fc6e304fcf17820d4e96
bryceshufro-build <bryceshufro@gmail.com>
2026-03-04 05:37:14 +0000

    only applicant or listing owner can view auth, messages shown are current user; auth must be part in thread, original sender can edit and delete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,11 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+
   def index
-    matching_messages = Message.all
+    # Only show messages in application threads the current user is part of
+    matching_messages = Message
+      .left_outer_joins(application: :listing)
+      .where("applications.user_id = ? OR listings.owner_id = ?", current_user.id, current_user.id)
 
     @list_of_messages = matching_messages.order({ :created_at => :desc })
 
@@ -10,25 +14,41 @@ class MessagesController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
+    @the_message = Message.where({ :id => the_id }).at(0)
 
-    matching_messages = Message.where({ :id => the_id })
+    # Authorization: only applicant OR listing owner can view
+    the_application = @the_message.application
+    allowed = (the_application.user_id == current_user.id) || (the_application.listing.owner_id == current_user.id)
 
-    @the_message = matching_messages.at(0)
+    if allowed == false
+      redirect_to("/listings", { :alert => "Not authorized." })
+      return
+    end
 
     render({ :template => "message_templates/show" })
   end
 
   def create
     the_message = Message.new
+
     the_message.application_id = params.fetch("query_application_id")
-    the_message.sender_id = params.fetch("query_sender_id")
+    the_message.sender_id = current_user.id
     the_message.body = params.fetch("query_body")
+
+    # Authorization: must be a participant in the thread
+    the_application = Application.where({ :id => the_message.application_id }).at(0)
+    allowed = (the_application.user_id == current_user.id) || (the_application.listing.owner_id == current_user.id)
+
+    if allowed == false
+      redirect_to("/listings", { :alert => "Not authorized." })
+      return
+    end
 
     if the_message.valid?
       the_message.save
-      redirect_to("/messages", { :notice => "Message created successfully." })
+      redirect_to("/applications/#{the_message.application_id}", { :notice => "Message sent." })
     else
-      redirect_to("/messages", { :alert => the_message.errors.full_messages.to_sentence })
+      redirect_to("/applications/#{the_message.application_id}", { :alert => the_message.errors.full_messages.to_sentence })
     end
   end
 
@@ -36,15 +56,19 @@ class MessagesController < ApplicationController
     the_id = params.fetch("path_id")
     the_message = Message.where({ :id => the_id }).at(0)
 
-    the_message.application_id = params.fetch("query_application_id")
-    the_message.sender_id = params.fetch("query_sender_id")
+    # Only the original sender can edit their message
+    if the_message.sender_id != current_user.id
+      redirect_to("/applications/#{the_message.application_id}", { :alert => "Not authorized." })
+      return
+    end
+
     the_message.body = params.fetch("query_body")
 
     if the_message.valid?
       the_message.save
-      redirect_to("/messages/#{the_message.id}", { :notice => "Message updated successfully." } )
+      redirect_to("/applications/#{the_message.application_id}", { :notice => "Message updated." })
     else
-      redirect_to("/messages/#{the_message.id}", { :alert => the_message.errors.full_messages.to_sentence })
+      redirect_to("/applications/#{the_message.application_id}", { :alert => the_message.errors.full_messages.to_sentence })
     end
   end
 
@@ -52,8 +76,13 @@ class MessagesController < ApplicationController
     the_id = params.fetch("path_id")
     the_message = Message.where({ :id => the_id }).at(0)
 
-    the_message.destroy
+    # Only the original sender can delete their message
+    if the_message.sender_id != current_user.id
+      redirect_to("/applications/#{the_message.application_id}", { :alert => "Not authorized." })
+      return
+    end
 
-    redirect_to("/messages", { :notice => "Message deleted successfully." } )
+    the_message.destroy
+    redirect_to("/applications/#{the_message.application_id}", { :notice => "Message deleted." })
   end
 end
