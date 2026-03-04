6d9155d601e7662ab3bf7c4f6182f4fad2ed777a
bryceshufro-build <bryceshufro@gmail.com>
2026-03-04 06:42:14 +0000

    added active storage for photos

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: applications
+#
+#  id               :bigint           not null, primary key
+#  decision_notes   :text
+#  message_to_owner :text
+#  priority_rank    :integer
+#  status           :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  listing_id       :integer
+#  user_id          :integer
+#
 class Application < ApplicationRecord
   belongs_to :listing
   belongs_to :user
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: listings
+#
+#  id           :bigint           not null, primary key
+#  address      :string
+#  available_on :date
+#  bathrooms    :decimal(, )
+#  bedrooms     :integer
+#  description  :text
+#  lease_end_on :date
+#  monthly_rent :integer
+#  neighborhood :string
+#  status       :string
+#  title        :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  owner_id     :integer
+#
 class Listing < ApplicationRecord
   belongs_to :owner, class_name: "User"
 
