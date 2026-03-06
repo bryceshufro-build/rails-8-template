e025fb9ef5c89e24fd6fa960b63de9d89a1fd1a7
bryceshufro-build <bryceshufro@gmail.com>
2026-03-06 18:59:14 +0000

    adding in actions for profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,23 @@
 Rails.application.routes.draw do
-  # Routes for the Message resource:
-
   post("/insert_message", { :controller => "messages", :action => "create" })
 
   get("/messages", { :controller => "messages", :action => "index" })
 
   get("/messages/:path_id", { :controller => "messages", :action => "show" })
 
-
   post("/modify_message/:path_id", { :controller => "messages", :action => "update" })
 
   get("/delete_message/:path_id", { :controller => "messages", :action => "destroy" })
 
-  #application
   post("/insert_application", { :controller => "applications", :action => "create" })
 
   get("/applications", { :controller => "applications", :action => "index" })
 
   get("/applications/:path_id", { :controller => "applications", :action => "show" })
 
-
   post("/modify_application/:path_id", { :controller => "applications", :action => "update" })
 
   get("/delete_application/:path_id", { :controller => "applications", :action => "destroy" })
-
-  # LISTINGS
 
   get("/listings/new", { :controller => "listings", :action => "new_form" })
   get("/listings", { :controller => "listings", :action => "index" })
@@ -36,6 +29,12 @@ Rails.application.routes.draw do
   get("/delete_listing_photo/:photo_id", { :controller => "listings", :action => "destroy_photo" })
 
   get("/listings/:path_id", { :controller => "listings", :action => "show" })
+
+  get("/profile", { :controller => "profiles", :action => "show" })
+  get("/profile/edit", { :controller => "profiles", :action => "edit_form" })
+  post("/modify_profile", { :controller => "profiles", :action => "update" })
+  get("/delete_profile", { :controller => "profiles", :action => "destroy" })
+
   devise_for :users
 
   get("/", { :controller => "pages", :action => "home" })
